a0d581cb01671dbb858ef89d7009f0dd289bd7c4
gaoxiaoliangz <gaoxiaoliangz@gmail.com>
2018-11-22 01:02:28 +0800

    -

--- src/App.jsx
+++ src/App.jsx
@@ -2,48 +2,51 @@ import React, { useRef, useEffect, useState } from 'react'
 import './App.scoped.scss'
 import MainTools from './components/MainTools/MainTools'
 
-let lastHandler = null
-
 const App = () => {
   const canvasRef = useRef(null)
   const { current: self } = useRef({})
   const [activeTool, setTool] = useState(null)
 
-  const handleMouseDown = e => {
-    self.isDown = true
+  self.state = {
+    activeTool,
   }
 
-  const handleMouseMove = () => {
-    if (self.isDown) {
-      console.log(activeTool)
-      if (activeTool === 'rect') {
-        console.log(self.ctx)
-        self.ctx.fillStyle = 'red'
-        self.ctx.fillRect(0, 0, 500, 500)
-      }
-      console.log('move')
+  const handleMouseDown = e => {
+    self.isDown = true
+    self.startDot = {
+      x: e.pageX,
+      y: e.pageY,
     }
   }
 
-  console.log(lastHandler === handleMouseMove)
-  console.log(lastHandler)
-  console.log(handleMouseMove)
-
-  lastHandler = handleMouseMove
+  const handleMouseMove = e => {
+    const { activeTool } = self.state
+    if (self.isDown) {
+      if (activeTool === 'rect') {
+        self.ctx.fillStyle = 'red'
+        self.ctx.clearRect(0, 0, 500, 500)
+        self.ctx.fillRect(
+          self.startDot.x,
+          self.startDot.y,
+          e.pageX - self.startDot.x,
+          e.pageY - self.startDot.y,
+        )
+      }
+    }
+  }
 
   const handleMouseUp = () => {
     self.isDown = false
   }
 
   useEffect(() => {
-    console.log('mounted')
     window.addEventListener('mousedown', handleMouseDown)
     window.addEventListener('mousemove', handleMouseMove)
     window.addEventListener('mouseup', handleMouseUp)
     self.ctx = canvasRef.current.getContext('2d')
 
     return () => {
-      console.log('unmount')
+      // @todo: clean up
     }
   }, [])
 
