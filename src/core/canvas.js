74af67e1559bca3ab1fa232d7493bdbaed3e41d8
Liang <gaoxiaoliangz@gmail.com>
2018-11-23 16:53:28 +0800

    opt

--- src/core/canvas.js
+++ src/core/canvas.js
@@ -1,4 +1,3 @@
-/* eslint-disable no-loop-func */
 const defaultCanvasConfig = {
   width: 500,
   height: 500,
@@ -10,6 +9,7 @@ const createCanvas = (config = {}) => {
   newCanvas.height = config.height || defaultCanvasConfig.height
   document.body.appendChild(newCanvas)
   newCanvas.setAttribute('class', config.class)
+  newCanvas.setAttribute('style', 'position: fixed; top: -999em;')
   return newCanvas
 }
 
@@ -20,42 +20,11 @@ const offscreenCanvases = {
   bufferCanvas: createCanvas({ class: 'bufferCanvas' }),
 }
 
-// @todo: config
-// const createCanvas = (config = defaultCanvasConfig, useNew = false) => {
-//   let newCanvas
-//   let newCtx
-//   if (!canvas || useNew) {
-//     newCanvas = document.createElement('canvas')
-//     newCanvas.width = config.width
-//     newCanvas.height = config.height
-//     document.body.appendChild(newCanvas)
-//     newCtx = newCanvas.getContext('2d')
-//     if (!useNew) {
-//       canvas = newCanvas
-//       ctx = newCtx
-//     }
-//     return newCanvas
-//   }
-//   return canvas
-// }
-
 /**
  * object
  * type: rect, x, y, width, height, color, stroke, radius?
  */
 export const objectToImageData = ({ type, ...rest }, canvasConfig) => {
-  // const canvasConfigWithDefaults = {
-  //   ...defaultCanvasConfig,
-  //   ...canvasConfig,
-  // }
-  // // ctx.clearRect(
-  // //   0,
-  // //   0,
-  // //   canvasConfigWithDefaults.width,
-  // //   canvasConfigWithDefaults.height
-  // // )
-
-  // createCanvas(canvasConfigWithDefaults)
   const canvas = offscreenCanvases.objectToImageData
   const ctx = canvas.getContext('2d')
   ctx.save()
@@ -77,87 +46,32 @@ export const objectToImageData = ({ type, ...rest }, canvasConfig) => {
   return imageData
 }
 
-// const fallback = (chain, finalFallback) => {
-//   for (let i = 0; i < chain.length; i++) {
-//     if (chain[i]) {
-//       return [chain[i], i]
-//     }
-//   }
-//   return [finalFallback, -1]
-// }
-
-// export const mergeImageData = (...imageDataArr) => {
-//   const [head, headIdx] = fallback(imageDataArr)
-//   if (!head) {
-//     return null
-//   }
-//   const newData = head.data.map((val, idx) => {
-//     return fallback(
-//       imageDataArr
-//         .slice(headIdx + 1)
-//         .filter(Boolean)
-//         .map(r => r.data[idx]),
-//       val
-//     )[0]
-//   })
-//   const newImgData = ctx.createImageData(canvas.width, canvas.height)
-//   newImgData.data.set(newData)
-//   return newImgData
-// }
-
-// const imageDataToDataURL = imageData => {
-//   // if (!dataUrlCanvas) {
-//   //   dataUrlCanvas = createCanvas(undefined, true)
-//   // }
-//   // console.log(imageData.data.filter(Boolean))
-//   // console.log('---')
-//   // const ctx = dataUrlCanvas.getContext('2d')
-
-//   const canvas = offscreenCanvases.imageDataToDataURL
-//   const ctx = canvas.getContext('2d')
-//   ctx.putImageData(imageData, 0, 0)
-//   return canvas.toDataURL('image/png')
-// }
+export const imageDataToDataURL = imageData => {
+  const canvas = offscreenCanvases.imageDataToDataURL
+  const ctx = canvas.getContext('2d')
+  ctx.putImageData(imageData, 0, 0)
+  return canvas.toDataURL('image/png')
+}
 
 const getCanvasWithImageData = imageData => {
   const canvas = offscreenCanvases.bufferCanvas
   const ctx = canvas.getContext('2d')
-  // ctx.clearRect(0, 0, canvas.width, canvas.height)
   ctx.putImageData(imageData, 0, 0)
   return canvas
 }
 
 export const mergeImageData = (...imageDataArr) => {
-  // createCanvas()
+  console.time(`merging ${imageDataArr.length}`)
   const canvas = offscreenCanvases.mergeImageData
   const ctx = canvas.getContext('2d')
   ctx.clearRect(0, 0, canvas.width, canvas.height)
   imageDataArr.forEach(imgData => {
-    if (!imgData || imgData.data.filter(Boolean).length === 0) {
+    if (!imgData) {
       return
     }
-    // const img = new Image()
-
-    // img.src = imageDataToDataURL(imgData)
     ctx.drawImage(getCanvasWithImageData(imgData), 0, 0)
-    // console.log(img)
-    // document.body.appendChild(img)
   })
-  return ctx.getImageData(0, 0, canvas.width, canvas.height)
-  // const [head, headIdx] = fallback(imageDataArr)
-  // if (!head) {
-  //   return null
-  // }
-  // const newData = head.data.map((val, idx) => {
-  //   return fallback(
-  //     imageDataArr
-  //       .slice(headIdx + 1)
-  //       .filter(Boolean)
-  //       .map(r => r.data[idx]),
-  //     val
-  //   )[0]
-  // })
-  // const newImgData = ctx.createImageData(canvas.width, canvas.height)
-  // newImgData.data.set(newData)
-  // return newImgData
+  console.timeEnd(`merging ${imageDataArr.length}`)
+  const result = ctx.getImageData(0, 0, canvas.width, canvas.height)
+  return result
 }
--- src/utils.js
+++ src/utils.js
@@ -3,3 +3,12 @@ export const reverse = arr => {
     return arr[arr.length - 1 - idx]
   })
 }
+
+export const fallback = (chain, finalFallback) => {
+  for (let i = 0; i < chain.length; i++) {
+    if (chain[i]) {
+      return [chain[i], i]
+    }
+  }
+  return [finalFallback, -1]
+}
